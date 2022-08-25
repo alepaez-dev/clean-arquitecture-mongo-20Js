c792e892e7c3a2968b8389993e4c404c78937162
alepaez-dev <alepaezdev@gmail.com>
2022-08-24 20:35:39 -0500

    feat: hash password

--- src/usecases/user.usecase.js
+++ src/usecases/user.usecase.js
@@ -1,8 +1,10 @@
 const User = require("../models/user.model")
+const bcrypt = require("bcrypt")
 
 // Crear un usuario
-const createUser = (userData) => {
-  const user = User.create(userData)
+const createUser = async (userData) => {
+  const hashPassword = await bcrypt.hash(userData.password, 10)
+  const user = User.create({...userData, password: hashPassword })
   return user
 }
 
