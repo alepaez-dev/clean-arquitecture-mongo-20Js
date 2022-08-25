55918e3ec6869f29169b3611be87f58ab009cc07
alepaez-dev <alepaezdev@gmail.com>
2022-08-24 21:36:00 -0500

    feat: login

--- src/usecases/auth.usecase.js
+++ src/usecases/auth.usecase.js
@@ -1,25 +1,19 @@
 const User = require("../models/user.model")
+const bcrypt = require("bcrypt")
+const createError = require("http-errors")
 
 // Login
 const login = async (email, textPlainPassword) => {
 
   // 1 - Es comprobar que el correo si sea con el de mi base de datos
   const user = await User.findOne({ email })
-  console.log("user:", user)
   // Si el usuario es null -> No me lo encontro -> No hay alguien con ese correo
-  if(!user) {
-    // Crear error
-    const error = new Error("No es el correo correcto")
-    console.log("error:", error)
-    error.status = 401
-    // Lanzar error
-    throw error
-  }
+  if(!user) throw createError(401, "No estas autorizado")
 
-  console.log("Si funciona")
-  // findOne
-
+  // si encontramos al user con ese correo
   // 2 - Es comprobar que la password si sea con el de mi base de datos
+  const isValidPassword = await bcrypt.compare(textPlainPassword, user.password)
+  if(!isValidPassword) throw createError(401, "No estas autorizado")
 }
 
 module.exports = {
